7ac97b2f894ef3fc931328d38390f3d6c578d607
Tu <tulkts2204023@fpt.edu.vn>
2023-03-07 18:29:48 +0700

    Tu - 2023/03/07 - 6:29pm

--- resources/views/fe/home/product.blade.php
+++ resources/views/fe/home/product.blade.php
@@ -12,12 +12,12 @@
         }
 
         /* .customer-review-option .comment-option.overflow-auto .co-item .avatar-text .at-role .bg-info{
-                            font-style: italic;
-                            font-size: 80%;
-                            font-weight: 500;
-                            text-shadow: 2px 2px 10px var(--violet-2nd);
-                        }
-                            /* ai rảnh chỉnh giùm em với, ko biết sao cho nó đẹp nữa */
+                                font-style: italic;
+                                font-size: 80%;
+                                font-weight: 500;
+                                text-shadow: 2px 2px 10px var(--violet-2nd);
+                            }
+                                /* ai rảnh chỉnh giùm em với, ko biết sao cho nó đẹp nữa */
 
 
         .personal-rating .btn-default,
@@ -166,7 +166,7 @@
                     <div class="row">
                         <div class="col-lg-6">
                             <div class="product-pic-zoom">
-                                <img src="{{ isset($item->oldestImage->url) ? asset('images/' . $product->oldestImage->url) : '' }}"
+                                <img src="{{ isset($product->oldestImage->url) ? asset('images/' . $product->oldestImage->url) : '' }}"
                                     alt="" class="product-big-img">
                                 <div class="zoom-icon">
                                     <i class="fa fa-search-plus"></i>
@@ -534,7 +534,7 @@
                     <div class="col-lg-3 col-sm-6">
                         <div class="product-item">
                             <div class="pi-pic">
-                                <img src="{{ isset($item->oldestImage->url) ? asset('images/' . $relate->oldestImage->url) : '' }}"
+                                <img src="{{ isset($relate->oldestImage->url) ? asset('images/' . $relate->oldestImage->url) : '' }}"
                                     alt="{{ $relate->name }}">
                                 <div class="sale pp-sale">Sale</div>
                                 <div class="icon">
@@ -567,15 +567,18 @@
 @section('myJs')
     <script type="module">
         import {CartHandler} from '{{ asset('/js/KienJs/cart.js') }}';
+        import { showSuccessToast, showErrorToast } from "{{ asset('/js/KienJs/toast.js') }}";
         document.addEventListener("readystatechange", (e) => {
             if (e.target.readyState === "complete") {
                 const addCart = new CartHandler({
                     url: '{{ Route('addCart') }}',
                     token: '{{ csrf_token() }}',
                     isUpdate: false,
-                    cartOrBtnSelector: ".pd-cart",
                     inputName: "product-quantity",
-                    headerCartSelector: ".cart-icon",
+                    selectors: {
+                        cartOrBtnSelector: ".pd-cart",
+                        headerCartSelector: ".cart-icon",
+                    }
                 });
             }
         });
