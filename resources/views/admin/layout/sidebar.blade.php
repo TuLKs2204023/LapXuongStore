b2653ffef9d8a712f1791fe0d2443c348f409d85
Tu <tulkts2204023@fpt.edu.vn>
2023-03-20 13:35:43 +0700

    Tu - 2023/03/20 - 1:35pm

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -19,11 +19,11 @@
         {{-- ---------------------------------------------------------start only Admin section--------------------------------------------------------------------- --}}
         @if (auth()->user()->role == 'Admin')
             <li class="nav-item">
-                <a class="nav-link" data-bs-target="#components-nav" data-bs-toggle="collapse" aria-expanded="true"
+                <a class="nav-link collapsed" data-bs-target="#users-nav" data-bs-toggle="collapse" aria-expanded="false"
                     href="#">
                     <i class="bi bi-person"></i><span>User Settings</span><i class="bi bi-chevron-down ms-auto"></i>
                 </a>
-                <ul id="components-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+                <ul id="users-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
                     <li>
                         <!-- User -->
                         <a href="{{ URL::to('/all-user') }}">
@@ -88,7 +88,7 @@
 
             <li class="nav-item">
                 <a class="nav-link " href="{{ Route('admin.rating.index') }}">
-                    <i class="bi bi-hand-thumbs-up-fill"></i>
+                    <i class="bi bi-hand-thumbs-up"></i>
                     <span>Rating</span>
                 </a>
             </li><!-- End Rating Nav -->
@@ -101,11 +101,11 @@
             </li><!-- End Rating Nav -->
 
             <li class="nav-item">
-                <a class="nav-link" data-bs-target="#specs-nav" data-bs-toggle="collapse" aria-expanded="true"
+                <a class="nav-link collapsed" data-bs-target="#specs-nav" data-bs-toggle="collapse" aria-expanded="false"
                     href="#">
                     <i class="bi bi-cpu"></i><span>Specifications</span><i class="bi bi-chevron-down ms-auto"></i>
                 </a>
-                <ul id="specs-nav" class="nav-content collapse show" data-bs-parent="#sidebar-nav">
+                <ul id="specs-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
                     <li>
                         <!-- Manufacture -->
                         <a href="{{ Route('admin.manufacture.index') }}">
